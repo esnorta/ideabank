9928ef6ab2bbe19355a3b241d296d16eb3fdca3c
rtl <timrezepov@yahoo.com>
2020-06-20 14:08:31 +0300

    настраиваю страница логина

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import  get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 class UserCreateForm(UserCreationForm):
 
@@ -18,4 +20,35 @@ class UserCreateForm(UserCreationForm):
         super().__init__(*args,**kwargs)
         self.fields['username'].label = 'Display Name'
         self.fields['email'].label = 'Email Address'
+
+class UserLoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+  #  username = forms.EmailField(widget=forms.TextInput(
+  #      attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'}))
+  #  password = forms.CharField(widget=forms.PasswordInput(
+  #      attrs={
+  #          'class': 'form-control',
+  #          'placeholder': '',
+  #          'id': 'hi',
+  #      }
+  #  ))
+
+    class Meta():
+        model = get_user_model()
+        fields = ('username', 'password')
+        widgets = {
+                'username': forms.TextInput(attrs = {'class': 'textinputclass',
+                        'type':'text', 'placeholder' : 'Название новой супер идеи'}),
+                'password': forms.TextInput(attrs = {'class': 'textinputclass',
+                        'placeholder': False}),
+            }
+
+        labels = {
+            'Username':'',
+            'Password':'',
+        }
+
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 # Django 1.11 introduced login and logout view. We dont have to create classes
-from . import views
+from django.contrib.auth import views as views_dj
+from accounts.forms import UserLoginForm
+from django.urls import path
+from accounts import views
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     url(r'login/$',
-        auth_views.LoginView.as_view(template_name='accounts/login.html'),
-            name = 'login'),
+        views_dj.LoginView.as_view(template_name='accounts/login.html',
+            authentication_form = UserLoginForm), name = 'login'),
     url(r'logout/$',
             auth_views.LogoutView.as_view(), name='logout'),
     url(r'signup/$', views.SignUp.as_view(), name='signup'),
